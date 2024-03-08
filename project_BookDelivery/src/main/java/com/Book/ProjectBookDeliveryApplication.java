0f3620d2ac1c2359403a6d01ea6095bb79dc6f66
seung-wo0 <lsw0919@gmail.com>
2024-03-08 19:28:02 +0900

    최초 셋팅업로드

---
+++ project_BookDelivery/src/main/java/com/Book/ProjectBookDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.Book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBookDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBookDeliveryApplication.class, args);
+	}
+
+}
